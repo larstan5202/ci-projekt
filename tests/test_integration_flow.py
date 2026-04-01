24cab090bb0e8ec6a6d7ae27af7ee0099a4e36b2
larstan5202 <lars.tangfelt@gmail.com>
2026-04-01 11:06:14 +0200

    Fix flake8 issues

--- tests/test_integration_flow.py
+++ tests/test_integration_flow.py
@@ -1,7 +1,7 @@
 import pytest
 from mypackage.core import add
 
-@pytest.mark.integration
+
 def test_integration_flow():
     result = add(10, 5)
     assert result == 15
--- tests/test_unit_core.py
+++ tests/test_unit_core.py
@@ -1,6 +1,7 @@
 import pytest
 from mypackage.core import add
-@pytest.mark.unit
 
+
+@pytest.mark.unit
 def test_add():
     assert add(2, 3) == 5
